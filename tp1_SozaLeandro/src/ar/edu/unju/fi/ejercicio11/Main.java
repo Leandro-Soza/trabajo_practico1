1c5025ada7470a7c7b9ebfa8c6b4bd8ea170180a
Leandro Soza <leancarp0495@gmail.com>
2024-04-15 22:24:03 -0300

    Ejercicio 11 resuelto

---
+++ tp1_SozaLeandro/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,12 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int i=2;
+		for(int j=40;j*i-(i*(i-2))>0;i++) {
+			System.out.print(j*i-(i*(i-2))+" ");
+		}
+	}
+
+}
